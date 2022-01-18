502f3421bc3eb007a5b8fb877bb0fadfbfbd8ef8
Jay <71242774+notsojay@users.noreply.github.com>
2022-01-18 01:37:20 -0800

    Update two_sum.cpp

--- CPP/Array/two_sum.cpp
+++ CPP/Array/two_sum.cpp
@@ -1,8 +1,12 @@
-/*
+// Source : https://leetcode.com/problems/two-sum/
+// Author : Jiahao Liang
+// Date   : 01-18-2022
+
+/*********************************************************************************** 
+
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 You can return the answer in any order.
-
 
 Example 1:
 Input: nums = [2,7,11,15], target = 9
@@ -22,8 +26,8 @@ Constraints:
 -109 <= nums[i] <= 109
 -109 <= target <= 109
 Only one valid answer exists.
-*/
 
+***********************************************************************************/ 
 
 /*
 Time Complexity:O(nlogn)
@@ -33,6 +37,7 @@ Time Complexity:O(n)
 每次判断的时候，我们将左右两个指针指向的元素加起来的和与我们要找的 target 对比，如果比 target 小，也就是说明如果在左指针不变的情况下，左指针加上左右指针中间的任意一个元素都会比 target 小，这也说明左指针不可能是我们要找的答案，因此向右移动左指针。
 如果是加起来和我们要找的 target 对比，比 target 大，分析类似，这时需要将右指针向左移动。
 */
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
