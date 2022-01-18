078fcbfafa955494677433c32f8d57bf262d7384
Jay <71242774+notsojay@users.noreply.github.com>
2022-01-18 00:59:38 -0800

    Update maximum_subarray.cpp

--- maximum_subarray.cpp
+++ maximum_subarray.cpp
@@ -1,9 +1,7 @@
 /*
 Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
-
 A subarray is a contiguous part of an array.
 
- 
 Example 1:
 Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
 Output: 6
@@ -17,12 +15,9 @@ Example 3:
 Input: nums = [5,4,-1,7,8]
 Output: 23
  
-
 Constraints:
 1 <= nums.length <= 105
 -104 <= nums[i] <= 104
-
-Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
 */
 
 
