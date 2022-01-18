e30d80f6b463cb3e4c33c15d09d8813e8d7db41e
Jay <71242774+notsojay@users.noreply.github.com>
2022-01-18 01:35:15 -0800

    Update maximum_subarray.cpp

--- CPP/Array/maximum_subarray.cpp
+++ CPP/Array/maximum_subarray.cpp
@@ -1,4 +1,9 @@
-/*
+// Source : https://leetcode.com/problems/maximum-subarray/
+// Author : Jiahao Liang
+// Date   : 01-18-2022
+
+/*********************************************************************************** 
+
 Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 A subarray is a contiguous part of an array.
 
@@ -18,8 +23,8 @@ Output: 23
 Constraints:
 1 <= nums.length <= 105
 -104 <= nums[i] <= 104
-*/
 
+***********************************************************************************/
 
 class Solution {
 public:
