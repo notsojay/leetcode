cfb2478ff12f57defe49f0c310377fd79edd0100
Jay <71242774+notsojay@users.noreply.github.com>
2022-01-18 01:33:44 -0800

    Update contains_duplicate.cpp

--- CPP/Array/contains_duplicate.cpp
+++ CPP/Array/contains_duplicate.cpp
@@ -1,4 +1,9 @@
-/*
+// Source : https://leetcode.com/problems/contains-duplicate/
+// Author : Jiahao Liang
+// Date   : 01-18-2022
+
+/*********************************************************************************** 
+
 Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
 Example 1:
@@ -16,7 +21,8 @@ Output: true
 Constraints:
 1 <= nums.length <= 105
 -109 <= nums[i] <= 109
-*/
+
+***********************************************************************************/
 
 
 class Solution {
