2c4c551a85760e3036fa97ae97ccedea0cb521a0
Jay <71242774+notsojay@users.noreply.github.com>
2022-04-19 19:46:51 -0700

    Create find_value_that_exists_in_both_arrays.cpp

---
+++ CPP/array/find_value_that_exists_in_both_arrays.cpp
@@ -0,0 +1,52 @@
+/*
+Given two sets A and B represented as sorted sequences, 
+describe an efficient algorithm for computing A⊕B, 
+which is the set of elements that are in A or B, but not in both.
+*/
+
+#include <iostream>
+#include <vector>
+
+class Solution 
+{
+public:
+	template<typename T>
+	std::vector<T> getResult(std::vector<T> nums1, std::vector<T> nums2)
+	{
+		std::vector<T> result;
+		auto i = nums1.begin(), j = nums2.begin();
+		std::sort(nums1.begin(), nums1.end());
+		std::sort(nums2.begin(), nums2.end());
+		while(i != nums1.end() && j != nums2.end())
+		{
+			if(*i == *j)
+			{
+				T temp = *i;
+				while(i != nums1.end() && *i == temp) ++i;
+				while(j != nums2.end() && *j == temp) ++j;
+			}
+			else if(*i > *j)
+			{
+				result.push_back(*j);
+				++j;
+			}
+			else if(*i < *j)
+			{
+				result.push_back(*i);
+				++i;
+			}
+		}
+		return result;
+	}
+};
+
+int main(int argc, char *argv[]) 
+{
+	Solution test;
+	std::vector<int> vec1 = {1, 3, 4, 6, 10, 5};
+	std::vector<int> vec2 = {4, 1, 5, 7, 9, 10};
+	std::vector<int> vec3 = test.getResult(vec1, vec2);
+	for(auto &i : vec3)
+		std::cout << i << " ";
+	return 0;
+}
